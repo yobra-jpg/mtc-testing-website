67a2d801ddd27f72560ec5c8cc9f459d2f82c10f
mercylifejpg <murayacae0026@kist.ac.ke>
2026-08-26 16:50:32 +0300

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,5 @@
+document.getElementById('logout').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Redirect back to the login page
+    window.location.href = "index.html"; // Change to your login page
+});
